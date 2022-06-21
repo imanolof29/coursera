f6e71db2639892e98e168becaf118967e47a8183
imanolortiz <imanolof29@gmail.com>
2022-06-21 23:52:24 +0200

    part 3

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def owner(request):
+       return HttpResponse("Hello, world. 475fe344 is the polls index.")
